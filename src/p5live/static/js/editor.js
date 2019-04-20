ec672b89d3b9b100f850b4ebf45d5eddf55f332e
cjayross <calvinjayross@gmail.com>
2019-04-20 01:10:14 -0500

    added a fake program to display in the demo by default

--- src/p5live/static/js/editor.js
+++ src/p5live/static/js/editor.js
@@ -1,5 +1,34 @@
 'use strict'
 
+const defaultText = `
+# Fake example program
+program = p5.Program(vertex, fragment)
+program['a_position'] = [(-1., -1.), (-1., +1.), (+1., -1.), (+1., +1.)]
+program['sphere_position_0'] = (.75, .1, 2.0 + cos(4*program.elapsed))
+program['sphere_radius_0'] = .6
+program['sphere_color_0'] = (0., 0., 1.)
+
+program['sphere_position_1'] = (-.75, .1, 2.0 - cos(4*program.elapsed))
+program['sphere_radius_1'] = .6
+program['sphere_color_1'] = (.5, .223, .5)
+
+program['plane_position'] = (0., -.5, 0.)
+program['plane_normal'] = (0., 1., 0.)
+
+program['light_intensity'] = 1.
+program['light_specular'] = (1., 50.)
+program['light_position'] = (5., 5., -10.)
+program['light_color'] = (1., 1., 1.)
+program['ambient'] = .05
+program['O'] = (0., 0., -1.)
+
+width, height = self.size
+p5.set_viewport(0, 0, *self.physical_size)
+program['u_aspect_ratio'] = width/float(height)
+
+show()
+`
+
 class Editor {
   constructor(element) {
     this.selfEl = $(element) || $(document.body);
@@ -10,7 +39,8 @@ class Editor {
         spellcheck: 'false',
         autocorrect: 'off',
         autocapitalize: 'off'
-      });
+      }).
+      html(defaultText);
     this.consoleEl = $('<div/>').
       addClass('editor-console');
     this.runEl = $('<button/>').
